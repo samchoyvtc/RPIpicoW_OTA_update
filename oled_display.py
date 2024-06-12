3e92668f4bf9a546e24df0315e0c6a66ac99fd19
Sam.Choy <87160016+samchoyvtc@users.noreply.github.com>
2024-06-12 19:50:24 +0800

    Update oled_display.py

--- oled_display.py
+++ oled_display.py
@@ -10,7 +10,17 @@ def hello_world():
     i2c_dev = I2C(0,scl=Pin(17),sda=Pin(16),freq=200000)  # start I2C on I2C1 (GPIO 26/27)
     oled = SSD1306_I2C(128, 64, i2c_dev) # oled controller
 
-    oled.text('SSD1306 Ready', 15, 12, 1)
-    oled.show()
+    count = 0
+    for _ in range(255):
+        str_count = str(count)
+        count += 1
+        oled.fill(0)  # Fill the screen with black (0 means off)
+        oled.text('SSD1306 Ready', 15, 12, 1)
+        oled.text(str_count, 15, 24, 1)
+        oled.show()
+        time.sleep(0.01)  # Wait for on_time seconds
+
+    
+    
     
     
