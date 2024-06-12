678fff8e4d3b3dfc3dc3c8ddfcd03a94c8c2b4ee
Sam.Choy <87160016+samchoyvtc@users.noreply.github.com>
2024-06-12 19:19:22 +0800

    Update oled_display.py

--- oled_display.py
+++ oled_display.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from machine import Pin,I2C
 
 # Add the 'libs' directory to the module search path
@@ -11,9 +12,10 @@ def hello_world():
 
     oled.text('SSD1306 Ready', 15, 12, 1)
     oled.show()
-    for _ in range():
-        pin.value(1)  # Turn the LED on
-        time.sleep(0.2)  # Wait for on_time seconds
-        pin.value(0)  # Turn the LED off
-        time.sleep(0.2)  # Wait for off_time seconds
+    count = 0
+    while True:
+        count_string = str(count)
+        oled.text(count_string, 15, 32, 1)
+        time.sleep(0.5)
+        count += 1
     
