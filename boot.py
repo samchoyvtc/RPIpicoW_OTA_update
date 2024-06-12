3f4b6d0532742556d1b62adbed2af582c745564d
Sam.Choy <87160016+samchoyvtc@users.noreply.github.com>
2024-06-12 11:13:21 +0800

    Added boot.py

---
+++ boot.py
@@ -0,0 +1,15 @@
+# Test Boot in ugit_test
+
+
+import machine
+import time
+
+print('hello ugit users!')
+
+print('I am working on an issue with ugit running into mem errors when connected to a computer')
+print('ugit seems to work well when battery powered or not in an active REPL session.')
+
+print('I hope you enjoy it!')
+    
+
+
