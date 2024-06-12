92380248e375741b5bbc0adf6300348054eb9c7a
Sam.Choy <87160016+samchoyvtc@users.noreply.github.com>
2024-06-12 11:22:20 +0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,13 +1,13 @@
 import ugit
 wlan = ugit.wificonnect()
-led = Pin(28, Pin.OUT)
+led_pin  = Pin(28, Pin.OUT)
 
 for _ in range(3):
-    led.value(1)  # Turn the LED on
+    led_pin .value(1)  # Turn the LED on
     time.sleep(0.5)   # Wait for 0.5 seconds
-    led.value(0)  # Turn the LED off
+    led_pin .value(0)  # Turn the LED off
     time.sleep(0.5)   # Wait for 0.5 seconds
 
-led.value(1)  # Turn the LED on
+led_pin .value(1)  # Turn the LED on
 ugit.pull_all()
-led.value(0)
+led_pin .value(0)
