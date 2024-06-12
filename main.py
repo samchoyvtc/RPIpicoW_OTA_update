0f44a2b46ec02e59092a4fa368daea793a5b4475
Sam.Choy <87160016+samchoyvtc@users.noreply.github.com>
2024-06-12 17:44:17 +0800

    added umqttsimple.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import sys
-from machine import Pin
+from machine import Pin, I2C
 import time
 
 # Add the 'libs' directory to the module search path
 sys.path.append('/Library')
-import ssd1306
+from ssd1306 import SSD1306_I2C
 
-def flash_led(pin, flashes, on_time, off_time):
-    """
-    Flash the LED a specified number of times.
-
-    :param pin: The Pin object for the LED
-    :param flashes: Number of times to flash the LED
-    :param on_time: Duration to keep the LED on (in seconds)
-    :param off_time: Duration to keep the LED off (in seconds)
-    """
+def flash_led(pin, flashes):
     for _ in range(flashes):
         pin.value(1)  # Turn the LED on
-        time.sleep(on_time)  # Wait for on_time seconds
+        time.sleep(0.2)  # Wait for on_time seconds
         pin.value(0)  # Turn the LED off
-        time.sleep(off_time)  # Wait for off_time seconds
+        time.sleep(0.2)  # Wait for off_time seconds
 
 # Define the onboard LED pin
-led_pin = Pin(15, Pin.OUT)
-# using default address 0x3C
-i2c = I2C(sda=Pin(18), scl=Pin(19))
-display = ssd1306.SSD1306_I2C(128, 64, i2c)
+led_pin = Pin(28, Pin.OUT)
+
+i2c_dev = I2C(0,scl=Pin(17),sda=Pin(16),freq=200000)  # start I2C on I2C1 (GPIO 26/27)
+oled = SSD1306_I2C(128, 64, i2c_dev) # oled controller
+
+
+oled.text('Hello World', 0, 0, 1)
+oled.show()
 
 # Flash the LED 3 times with 0.5 seconds on and 0.5 seconds off
-flash_led(led_pin, 3, 0.5, 0.5)
+flash_led(led_pin, 5)
